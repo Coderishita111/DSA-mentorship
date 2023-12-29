e5c7e33cdef2c3da3acd18b7bf7ae50fb8012843
Ishita Mahansaria <144829963+Coderishita111@users.noreply.github.com>
2023-12-29 23:11:30 +0530

    Create A1_q1.java

---
+++ A1_q1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class prime
+{
+public static void main (String args[])
+{
+System.out.println("The pattern Z:");
+System.out.println("*****");
+System.out.println("     *");
+System.out.println("   *");
+System.out.println(" *");
+System.out.println("*****");
+}
+}
