4024a0f5104d6a142c61793a9ca0d85d94abf08c
Ishita Mahansaria <144829963+Coderishita111@users.noreply.github.com>
2024-01-06 16:36:53 +0530

    Create A2_q4.java

---
+++ A2_q4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class pattern
+{
+public static void main (String args[])
+{
+int n,i,k,j;
+Scanner sc= new Scanner(System.in);
+System.out.println("Enter a number");
+n= sc.nextInt();
+for(i=n;i>=1;i--)
+{
+ for(j=1:j<=n-i;j++)
+{
+System.out.print(" ");
+}
+for(k=1;k<=i;k++)
+System.out.print("*");
+System.out.println();
+}
+}
+}
